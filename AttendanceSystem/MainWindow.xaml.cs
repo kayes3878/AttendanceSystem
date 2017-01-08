17875cdeba82a5db7435550e6bfd493eb463d97e
kayes3878 <kayes3878@gmail.com>
2017-01-08 19:00:17 +0600

    SignalR

--- AttendanceSystem/MainWindow.xaml.cs
+++ AttendanceSystem/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using AttendanceSystem.Entity;
 using DATA;
+using Microsoft.AspNet.SignalR.Client;
 
 
 
@@ -25,6 +26,11 @@ namespace AttendanceSystem
     public partial class MainWindow : Window
     {
         AttendanceSystemDBEntities attendanceSystemDBEntitiesObj;
+
+        public String UserName { get; set; }
+        public IHubProxy HubProxy { get; set; }
+        const string ServerURI = "http://localhost:8080/signalr";
+        public HubConnection Connection { get; set; }
         public MainWindow()
         {
             InitializeComponent();
@@ -34,7 +40,44 @@ namespace AttendanceSystem
             LoadAllEmployee();
 
         }
+        private async void ConnectAsync()
+        {
+            Connection = new HubConnection(ServerURI);
+            Connection.Closed += Connection_Closed;
+            HubProxy = Connection.CreateHubProxy("MyHub");
+            //Handle incoming event from server: use Invoke to write to console from SignalR's thread
+            HubProxy.On<string, string>("AddMessage", (name, message) =>
+                this.Dispatcher.Invoke(() =>
+                    //RichTextBoxConsole.AppendText(String.Format("{0}: {1}\r", name, message))
+                )
+            );
+            try
+            {
+                await Connection.Start();
+            }
+            catch (HttpRequestException)
+            {
+                //StatusText.Content = "Unable to connect to server: Start server before connecting clients.";
+                //No connection: Don't enable Send button or show chat UI
+                return;
+            }
 
+            //Show chat UI; hide login UI
+            //SignInPanel.Visibility = Visibility.Collapsed;
+            //ChatPanel.Visibility = Visibility.Visible;
+            //ButtonSend.IsEnabled = true;
+            //TextBoxMessage.Focus();
+            //RichTextBoxConsole.AppendText("Connected to server at " + ServerURI + "\r");
+        }
+        void Connection_Closed()
+        {
+            //Hide chat UI; show login UI
+            //var dispatcher = Application.Current.Dispatcher;
+            //dispatcher.Invoke(() => ChatPanel.Visibility = Visibility.Collapsed);
+            //dispatcher.Invoke(() => ButtonSend.IsEnabled = false);
+            //dispatcher.Invoke(() => StatusText.Content = "You have been disconnected.");
+            //dispatcher.Invoke(() => SignInPanel.Visibility = Visibility.Visible);
+        }
         private void LoadAllEmployee()
         {
             List<AttendanceSystem.Entity.EmployeeInfo> items = new List<AttendanceSystem.Entity.EmployeeInfo>();
